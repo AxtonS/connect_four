3b1e5b5b59400a2486d33d27ebc94054a664fc81
Axton Smith <axtonsmith@gmail.com>
2023-06-25 16:37:07 -0400

    Add board attribute to board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
-# contains board state and receives player moves
+# contains board state, places pieces, and checks for win condition
 class Board
+  attr_accessor :board
+
+  def initialize
+    @board = { A1: 0, A2: 0, A3: 0, A4: 0, A5: 0, A6: 0, A7: 0,
+               B1: 0, B2: 0, B3: 0, B4: 0, B5: 0, B6: 0, B7: 0,
+               C1: 0, C2: 0, C3: 0, C4: 0, C5: 0, C6: 0, C7: 0,
+               D1: 0, D2: 0, D3: 0, D4: 0, D5: 0, D6: 0, D7: 0,
+               E1: 0, E2: 0, E3: 0, E4: 0, E5: 0, E6: 0, E7: 0,
+               F1: 0, F2: 0, F3: 0, F4: 0, F5: 0, F6: 0, F7: 0 }
+  end
+
+  def state
+    
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,17 +4,19 @@ require './lib/board'
 
 describe Board do
   describe '#initialize' do
-    subject = Board.new
     it 'should create board object' do
       expect(subject).to be_an_instance_of Board
+    end
+    context 'when empty' do
+      it 'A1 space should be empty' do
+        expect(subject.board[:A1]).to eql(0)
+      end
+      it 'F7 space should be empty' do
+        expect(subject.board[:F7]).to eql(0)
+      end
     end
   end
 
   describe '#state' do
-    context 'when empty' do
-      it 'returns an empty board' do
-        expect(subject.state).to eql
-      end
-    end
   end
 end
