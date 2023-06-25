96dd3c73dd30affe4204a5855b98e9530afc3c34
Axton Smith <axtonsmith@gmail.com>
2023-06-25 16:00:45 -0400

    Create board and board_spec

---
+++ lib/board.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# contains board state and receives player moves
+class Board
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
-describe 'Board'
+require './lib/board'
+
+describe Board do
+  describe '#initialize' do
+    subject = Board.new
+    it 'should create board object' do
+      expect(subject).to be_an_instance_of Board
+    end
+  end
+
+  describe '#state' do
+    context 'when empty' do
+      it 'returns an empty board' do
+        expect(subject.state).to eql
+      end
+    end
+  end
+end
